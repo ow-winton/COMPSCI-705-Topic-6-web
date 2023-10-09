84e21c15a389c7882becebdd9a5edbdeb73deeb2
1006413349 <65016202+1006413349@users.noreply.github.com>
2023-10-09 15:58:43 +1300

    new functions
    tag,download,allows txt upload

---
+++ web/db_test.php
@@ -0,0 +1,10 @@
+<?php
+//    $host = 'sql210.bytecluster.com';
+//    $user = 'if0_34966954';
+//    $password = 'CbyXRcHCPk';
+//    $db = 'if0_34966954_cs705_web_app_db';
+    $host = 'localhost';
+    $user = 'ss';
+    $password = '123123';
+    $db = 'ss';
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
